854ca9d622c9b2e081186a66db2c053df1ca903d
olivial24 <olivia.lorenz24@gmail.com>
2018-07-25 18:15:29 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,7 +7,7 @@ student_Names = [
   "Rihanna"
 ]
 
- student_Names.each do "#{0,1,2}"
+ student_Names.each "#{0,1,2}"
  
 
   # Write a solution that returns the first student in the array student_names
